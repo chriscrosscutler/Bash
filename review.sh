3b7d0321d5fac6404f372080d4f63ada10b339e0
Chriscrosscutler <chris.b.cutler@gmail.com>
2016-09-21 14:47:18 -0600

    added review script

---
+++ review.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# A simple bash script that saves the BrainExtractionBrain.nii.gz files to a new folder in your subject directory.
+# This folder is then rsync'd to your desktop where a for loop opens each image in quick look, 
+# takes a screenshot and saves it to a new folder. The resulting folder could then be opened in Preview to 
+# quickly scan through all the subjects or saved to a PDF for future reference.
+
+###################
+# CHANGE THESE!!! #
+###################
+datalocation=/fslhome/ccutle25/compute/Repeatability/Dehydration/Rerun/ #Folder where your subjects are located 
+savelocation=/fslhome/ccutle25/compute/Repeatability/Dehydration/ #Where you want the preview PDF to be saved
+
+#generate images
+for subj in $(ls $datalocation); do
+echo $subj
+subjdir=/fslhome/ccutle25/compute/Repeatability/Dehydration/$subj
+/fslhome/ccutle25/apps/c3d/bin/c3d ${subjdir}/BrainExtractionBrain.nii.gz -slice x 50% -type uchar -o $subjdir/x.png
+/fslhome/ccutle25/apps/c3d/bin/c3d ${subjdir}/BrainExtractionBrain.nii.gz -slice y 50% -type uchar -o $subjdir/y.png
+/fslhome/ccutle25/apps/c3d/bin/c3d ${subjdir}/BrainExtractionBrain.nii.gz -slice z 50% -type uchar -o $subjdir/z.png
+convert -flip $subjdir/x.png $subjdir/x.png
+convert -flip $subjdir/y.png $subjdir/y.png
+montage ${subjdir}/x.png ${subjdir}/y.png ${subjdir}/z.png -mode concatenate -tile 2x2 ${subj}.png
+rm $subjdir/x.png
+rm $subjdir/y.png
+rm $subjdir/z.png
+done
+
+#move them to a new folder
+mkdir /fslhome/ccutle25/compute/Repeatability/Dehydration/preview/
+for sub in $(ls /fslhome/ccutle25/compute/Repeatability/Dehydration/); do
+echo $sub
+subjdir=/fslhome/ccutle25/compute/Repeatability/Dehydration/$sub
+mv $subjdir/${sub}.png /fslhome/ccutle25/compute/Repeatability/Dehydration/preview
+done
+
+#create PDF
+cd /fslhome/ccutle25/compute/Repeatability/Dehydration/preview/
+convert *.png -gravity South -annotate 0 '%f' ${savelocation}preview.pdf
